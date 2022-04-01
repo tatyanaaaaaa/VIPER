12a0b9bbdd8ce9c43fc69fac930842055624048c
Sosina Tatyana <fedorova_t95@mail.ru>
2022-04-02 01:21:37 +0300

    Initial Commit

---
+++ VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER
+//
+//  Created by Tatiana Sosina on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
