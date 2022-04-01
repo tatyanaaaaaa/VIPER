24546047b3a6836a4c4d314468e59ea8bb7713c3
Sosina Tatyana <fedorova_t95@mail.ru>
2022-04-02 02:22:47 +0300

    Add Interactor

---
+++ VIPER/UserStory/Modules/MainScreen/Interactor/MainScreenInteractor.swift
@@ -0,0 +1,42 @@
+//
+//  MainScreenInteractor.swift
+//  VIPER
+//
+//  Created by Tatiana Sosina on 02.04.2022.
+//
+
+import UIKit
+
+/// Интерактор `Главный экран`
+protocol MainScreenInteractorInput {
+    
+    /// Загрузить контент
+    func getContent()
+}
+
+/// Обработчик событий от интерактора
+protocol MainScreenInteractorOutput: AnyObject {
+    
+    /// `Были получены данные`
+    /// - Parameter text: текст
+    func didReceive(text: String)
+}
+
+/// Интерактор `Экрана выбора банка получателя`
+final class MainScreenInteractor: MainScreenInteractorInput {
+    
+    weak var output: MainScreenInteractorOutput?
+    
+    func getContent() {
+        let appearance = Appearance()
+        output?.didReceive(text: appearance.someText)
+    }
+}
+
+// MARK: - Appearance
+
+private extension MainScreenInteractor {
+    struct Appearance {
+        let someText = "Hello, world"
+    }
+}
